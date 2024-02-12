ed9c20cf91f93b89ef12d1c827da1a7615d99091
aliciane-pinault <104615235+aliciane-pinault@users.noreply.github.com>
2024-02-12 16:30:20 +0100

    ajout chrono QR code + debut implementation lien firebase (#14)
    * ajout chrono QR code + debut implementation lien firebase
    
    * ajout lien firebase avec QRcode + clean code

--- EducScan/app/build.gradle.kts
+++ EducScan/app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation ("com.journeyapps:zxing-android-embedded:4.2.0")
     implementation ("com.google.zxing:core:3.4.0")
     implementation("com.androidplot:androidplot-core:1.5.10")
+    implementation("androidx.media3:media3-common:1.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
