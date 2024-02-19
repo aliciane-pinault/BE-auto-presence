25cdbd7c93a2ceb220f7a4d5feff327715777dce
unknown <MathildeH-28>
2024-02-19 10:23:12 +0100

    ajout pour le choix de la langue côté prof mais ça marche toujours pas

--- EducScan/app/src/main/java/fr/isen/perigot/educscan/ProfActivity.java
+++ EducScan/app/src/main/java/fr/isen/perigot/educscan/ProfActivity.java
@@ -1,7 +1,10 @@
 package fr.isen.perigot.educscan;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -9,14 +12,20 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.Locale;
+
 import fr.isen.perigot.educscan.databinding.ActivityMainBinding;
+import fr.isen.perigot.educscan.R;
 import fr.isen.perigot.educscan.databinding.ActivityProfBinding;
 
 public class ProfActivity extends AppCompatActivity {
 
     private ActivityProfBinding binding;
+    private static final int MENU_LANGUAGE_FR = 1;
+    private static final int MENU_LANGUAGE_EN = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +50,41 @@ public class ProfActivity extends AppCompatActivity {
                 .build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navViewProf, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Gonflez le menu de traduction dans l'action bar
+        getMenuInflater().inflate(R.menu.menu_trad, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_LANGUAGE_FR:
+                setLocale("fr");
+                return true;
+            case MENU_LANGUAGE_EN:
+                setLocale("en");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void setLocale(String lang) {
+        // Créer un objet de type Locale avec la langue spécifiée
+        Locale locale = new Locale(lang);
+        // Définir la locale pour l'application
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getResources().updateConfiguration(config, getResources().getDisplayMetrics());
+
+        // Redémarrer l'activité pour appliquer les changements de langue
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
     }
 }
